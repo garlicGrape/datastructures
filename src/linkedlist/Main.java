a0f6f514de8eaaa10caa685c4623b4a255f1a004
garlicGrape <sk7374@nyu.edu>
2023-06-12 00:55:48 -0400

    Linked List files

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -1,4 +1,19 @@
 package linkedlist;
 
 public class Main {
+
+    public static void main(String[] args) {
+        /*LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.printList();*/
+
+        LinkedList myLinkedList = new LinkedList(1);
+
+        myLinkedList.append(2);
+        myLinkedList.printList();
+    }
 }
